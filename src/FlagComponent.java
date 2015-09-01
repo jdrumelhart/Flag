b13dfdf71d6260a67a1b22e99b0224d8ad31426d
jdrumelhart <jdrumelhart@yahoo.com>
2015-09-01 11:12:28 -0700

    Got old glory colors

--- src/FlagComponent.java
+++ src/FlagComponent.java
@@ -3,7 +3,8 @@ import javax.swing.*;
 
 public class FlagComponent extends JComponent {
 	private Dimension d;
-	
+	private final Color OGRED = new Color(0xBB133E);
+	private final Color OGBLUE = new Color(0x002664);
 	//Default constructor
 	FlagComponent() {
 		d = new Dimension();
@@ -17,6 +18,7 @@ public class FlagComponent extends JComponent {
 	/*
 	 * paint method called whenever window is resized
 	 * draws images on screen
+	 * NOTE: look up OG colors red and blue and implement them
 	 * @see javax.swing.JComponent#paint(java.awt.Graphics)
 	 */
 	public void paint(Graphics g) {
@@ -32,13 +34,13 @@ public class FlagComponent extends JComponent {
 		g.setColor(Color.WHITE);
 		//Creates the white background
 		g.fillRect(0,0,(int)(scalar * 1.9), scalar);
-		g.setColor(Color.RED);
+		g.setColor(OGRED);
 		//Creates red stripes
 		for(int i = 0; i <= 6; i++) {
 			g.fillRect(0,(int) ((2 * i * scalar + 1)/13), (int)(scalar * 1.9), (int) (scalar/13));
 		}
 		//Creates the blue Union in the upper corner
-		g.setColor(Color.BLUE);
+		g.setColor(OGBLUE);
 		g.fillRect(0,0,(int)(scalar * 0.76), (int) ((scalar/13) * 7));
 	}
 }
