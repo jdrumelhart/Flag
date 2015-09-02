9763844ed5e2dd870ecd0b2bf920b16a6e0a19e3
jdrumelhart <jdrumelhart@yahoo.com>
2015-09-01 21:34:27 -0700

    began work on stars

--- src/FlagComponent.java
+++ src/FlagComponent.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import java.math.*;
 
 public class FlagComponent extends JComponent {
 	private Dimension d;
@@ -23,27 +24,16 @@ public class FlagComponent extends JComponent {
 	public void paint(Graphics g) {
 		//Sets the d object to the current size of the window
 		d.setSize(getWidth(), getHeight());
-<<<<<<< HEAD
-<<<<<<< HEAD
-		g.setColor(Color.WHITE);
-		g.fillRect(0,0,(int) (1.9 * d.getHeight()),(int) d.getHeight());
-		g.setColor(Color.RED);
-		for(int i = 0; i <= 6; i++) {
-			g.fillRect(0,(int) ((2 * i * d.getHeight() + 1)/13), (int)(d.getHeight() *1.9), (int) (d.getHeight()/13));
-		}
-		g.setColor(Color.BLUE);
-		g.fillRect(0,0,(int) ((d.getHeight()/13) * 7), (int)(d.getHeight() * 0.76 * 1.9));
-=======
-		int scalar = 0;
+		double scalar = 0;
 		if(d.getHeight() > (d.getWidth() / 1.9)) {
-			scalar = (int) (d.getWidth() / 1.9);
+			scalar = (d.getWidth() / 1.9);
 		}
 		else {
-			scalar = (int) d.getHeight();
+			scalar = d.getHeight();
 		}
 		g.setColor(Color.WHITE);
 		//Creates the white background
-		g.fillRect(0,0,(int)(scalar * 1.9), scalar);
+		g.fillRect(0,0,(int)(scalar * 1.9), (int)scalar);
 		g.setColor(OGRED);
 		//Creates red stripes
 		for(int i = 0; i <= 6; i++) {
@@ -52,26 +42,21 @@ public class FlagComponent extends JComponent {
 		//Creates the blue Union in the upper corner
 		g.setColor(OGBLUE);
 		g.fillRect(0,0,(int)(scalar * 0.76), (int) ((scalar/13) * 7));
->>>>>>> origin/master
-=======
-		int scalar = 0;
-		if(d.getHeight() > (d.getWidth() / 1.9)) {
-			scalar = (int) (d.getWidth() / 1.9);
-		}
-		else {
-			scalar = (int) d.getHeight();
-		}
 		g.setColor(Color.WHITE);
-		//Creates the white background
-		g.fillRect(0,0,(int)(scalar * 1.9), scalar);
-		g.setColor(OGRED);
-		//Creates red stripes
-		for(int i = 0; i <= 6; i++) {
-			g.fillRect(0,(int) ((2 * i * scalar + 1)/13), (int)(scalar * 1.9), (int) (scalar/13));
-		}
-		//Creates the blue Union in the upper corner
-		g.setColor(OGBLUE);
-		g.fillRect(0,0,(int)(scalar * 0.76), (int) ((scalar/13) * 7));
->>>>>>> origin/master
+		double centerX = 0.063 * scalar;
+		double centerY = 0.054 * scalar;
+		int[] xPoints = new int[5];
+		int[] yPoints = new int[5];
+		xPoints[0] = (int)centerX;
+		yPoints[0] = (int)(centerY - scalar * 0.0616);
+		xPoints[1] = (int)(centerX - scalar * 0.0616 * Math.cos(Math.PI / 10));
+		yPoints[1] = (int)(centerY + scalar * 0.0616 * Math.sin(Math.PI / 10));
+		xPoints[2] = (int)(centerX - scalar * 0.0616 * Math.cos(3 * Math.PI / 10));
+		yPoints[2] = (int)(centerY + scalar * 0.0616 * Math.sin(3 * Math.PI / 10));
+		xPoints[3] = (int)(centerX + scalar * 0.0616 * Math.cos(3 * Math.PI / 10));
+		yPoints[3] = (int)(centerY - scalar * 0.0616 * Math.sin(3 * Math.PI / 10));
+		xPoints[4] = (int)(centerX - scalar * 0.0616 * Math.cos(Math.PI / 10));
+		yPoints[4] = (int)(centerY + scalar * 0.0616 * Math.sin(Math.PI / 10));
+		g.fillPolygon(xPoints, yPoints, 5);
 	}
 }
